3c389ad600e255a6d38f69c21ac953ba8fd416ed
Zander Toews <zptoews@gmail.com>
2023-11-24 14:19:22 -0600

    trying to make the divs format good/truobble shooting css style

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,20 +3,35 @@
 import { createContact } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+    let contactCount = 0;
+
     document.querySelector('.submit').addEventListener('click', function() {
-        const newContactInfo = createContact();
+        if (contactCount < 9) {
+            const newContactInfo = createContact();
+            const contactPage = document.querySelector('.contactPage');
 
-        const contactPage = document.querySelector('.contactPage');
-        const newContactDiv = document.createElement('div');
-        newContactDiv.classList.add('contactBox');
+            let currentRow = document.querySelector('.contactRow:last-child');
 
-        const formattedInfo = `
-            <p><strong>Name:</strong> ${newContactInfo.fullName}</p>
-            <p><strong>City:</strong> ${newContactInfo.city}</p>
-            <p><strong>Email:</strong> ${newContactInfo.email}</p>
-        `;
-        newContactDiv.innerHTML = formattedInfo;
+            if (!currentRow || currentRow.children.length === 3) {
+                currentRow = document.createElement('div');
+                currentRow.classList.add('contactRow');
+                contactPage.appendChild(currentRow);
+            }
 
-        contactPage.appendChild(newContactDiv);
+            const newContactDiv = document.createElement('div');
+            newContactDiv.classList.add('contactBox');
+
+            const formattedInfo = `
+                <p class="contactText"><strong>Name:</strong> ${newContactInfo.fullName}</p>
+                <p class="contactText"><strong>City:</strong> ${newContactInfo.city}</p>
+                <p class="contactText"><strong>Email:</strong> ${newContactInfo.email}</p>
+            `;
+            newContactDiv.innerHTML = formattedInfo;
+
+            currentRow.appendChild(newContactDiv);
+            contactCount++;
+        } else {
+            alert("You've reached the limit of 9 contacts (3x3 grid)!");
+        }
     });
 });
